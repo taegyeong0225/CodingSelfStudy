46486dba0b3315396d87ea728e295b08ffc52e19
taegyeong0225 <202244042@itc.ac.kr>
2023-07-04 01:26:15 +0900

    python day1, 2

---
+++ day2_parcel_state.py
@@ -0,0 +1,8 @@
+status = "상품 준비"
+print("주문상태 :", status)
+
+status = "배송 중"
+print("주문상태 :", status)
+
+status = "배송 완료"
+print("주문상태 :", status)
---
+++ day2_station_print.py
@@ -0,0 +1,6 @@
+station = "사당"
+print(station)
+station = "신도림"
+print(station)
+station = "인천공항"
+print(station)
